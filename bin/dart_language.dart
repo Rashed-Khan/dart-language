f594ca3e9387c95a5901da406f6b45ce6763ba1a
Rashed-Khan <eng.rashedmilon002@gmail.com>
2025-11-27 09:45:51 +0600

    Default Constractor

--- bin/dart_language.dart
+++ bin/dart_language.dart
@@ -1,21 +1,17 @@
 class Student {
-  String? name;
-  int? age;
   void display() {
-    print(name);
-    print(age);
+    print('I am display Method');
   }
-  Student(){
-    print('Default Constractor Milon ');
+
+  Student() {
+    print('I am Default Constractor');
   }
 }
 
 void main() {
-  var std1 = Student();
-  std1.name = "Rashed";
-  std1.age = 30;
-
-
+  var std1 = Student(); // Default Constractor
   std1.display();
-  Student();
+
+
+
 }
